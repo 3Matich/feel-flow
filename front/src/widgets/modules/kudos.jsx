ab93c6f143490be648db1439ebf556c20b43d5a2
JuanZozaya <juanpablozozaya2014@gmail.com>
2025-02-25 19:38:15 -0300

    modulos_desc

--- front/src/widgets/modules/kudos.jsx
+++ front/src/widgets/modules/kudos.jsx
@@ -12,7 +12,6 @@ import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import { DateTimePicker } from "@mui/x-date-pickers/DateTimePicker";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import dayjs from "dayjs";
 
 import { useMaterialTailwindController } from "@/context";
 
@@ -26,50 +25,43 @@ export function Kudos() {
     const { sidenavColor } = controller;
 
     const kudosIcons = [
-        "/icons/Kudos_Energia_Positiva.svg",
-        "/icons/Kudos_Resolutor_Estrella.svg",
-        "/icons/Kudos_Maestro_Del_Detalle.svg",
-        "/icons/Kudos_Manos_Amigas.svg"
+        "/public/icons/Kudos_Energia_Positiva.svg",
+        "/public/icons/Kudos_Resolutor_Estrella.svg",
+        "/public/icons/Kudos_Maestro_Del_detalle.svg",
+        "/public/icons/Kudos_Manos_Amigas.svg"
     ];
 
     const kudosDescriptions = [
-        "Kudos Energía Positiva: Reconocemos a quienes contagian entusiasmo y motivación al equipo.",
-        "Kudos Resolutor Estrella: Celebramos a quienes encuentran soluciones rápidas y efectivas a los problemas.",
-        "Kudos Maestro del Detalle: Valoramos a aquellos que prestan atención a cada pequeño detalle para garantizar la calidad.",
-        "Kudos Manos Amigas: Apreciamos a quienes siempre están dispuestos a ayudar a sus compañeros de equipo."
+        "Energía Positiva: Para los que contagian entusiasmo y motivación al equipo.",
+        "Resolutor Estrella: Para los que encuentran soluciones efectivas a los problemas.",
+        "Maestro del Detalle: Para los que prestan atención a cada pequeño detalle.",
+        "Manos Amigas: Para los que siempre ayudan a sus compañeros."
     ];
 
-    console.log("Kudos Icons:", kudosIcons);
-    console.log("Kudos Descriptions:", kudosDescriptions);
-
     return (
-        <div className="flex gap-7">
-            <Card color="transparent" shadow={false}>
-                <Typography variant="h4" color="blue-gray">
+        <div className="flex gap-7 items-start">
+            <Card color="transparent" shadow={false} className="w-1/2 p-5 flex flex-col justify-start">
+                <Typography variant="h4" color="blue-gray" className="mb-2 text-center">
                     Configurar Módulo de Kudos
                 </Typography>
-                <Typography color="gray" className="mt-1 font-normal">
+                <Typography color="gray" className="font-normal text-center">
                     Seleccione las opciones para configurar el módulo.
                 </Typography>
-                <form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96">
-                    <div className="mb-1 flex flex-col gap-6">
-                        <Typography variant="h6" color="blue-gray" className="-mb-3">
+                <form className="mt-4">
+                    <div className="mb-1 flex flex-col gap-4">
+                        <Typography variant="h6" color="blue-gray">
                             Equipo
                         </Typography>
-                        <Select
-                            value={teamValue}
-                            onChange={(val) => setTeamValue(val)}
-                            disabled
-                        >
+                        <Select disabled>
                             <Option value="Team 1">Los Umpalumpas</Option>
                             <Option value="Team 2">Real Alcohólicos</Option>
                             <Option value="Team 3">Funcopops</Option>
                         </Select>
-                        <Typography variant="h6" color="blue-gray" className="-mb-3">
+                        <Typography variant="h6" color="blue-gray">
                             Fecha de habilitación del módulo
                         </Typography>
                         <Input type="date" size="lg" className="!border-t-blue-gray-200 focus:!border-t-gray-900" />
-                        <Typography variant="h6" color="blue-gray" className="-mb-3">
+                        <Typography variant="h6" color="blue-gray">
                             Fecha de Fin del módulo
                         </Typography>
                         <Input type="date" size="lg" className="!border-t-blue-gray-200 focus:!border-t-gray-900" />
@@ -84,24 +76,24 @@ export function Kudos() {
                             </LocalizationProvider>
                         </div>
                     </div>
-                    <Button className="mt-6" fullWidth color={sidenavColor}>
+                    <Button className="mt-4" fullWidth color={sidenavColor}>
                         Guardar
                     </Button>
                 </form>
             </Card>
-            <Card color="transparent" shadow={false} className="w-96 p-5 flex flex-col items-center mx-auto">
-                <Typography variant="h4" color="blue-gray" className="mb-3 text-center">
+            <Card color="transparent" shadow={false} className="w-1/2 p-5 flex flex-col items-center">
+                <Typography variant="h4" color="blue-gray" className="mb-2 text-center">
                     Módulo de Kudos
                 </Typography>
-                <Typography color="gray" className="mt-1 font-normal mb-4 text-center">
-                    Este módulo permite otorgar kudos para incentivar el reconocimiento y motivación del equipo.
+                <Typography color="gray" className="font-normal text-center mb-4">
+                    Incentivamos el reconocimiento y motivación del equipo.
                 </Typography>
-                <div className="flex flex-col gap-4 w-full">
+                <div className="grid grid-cols-2 gap-4 w-full">
                     {kudosIcons.map((icon, index) => (
-                        <div key={index} className="flex items-center gap-4 w-full p-2">
-                            <img src={icon} alt={kudosDescriptions[index].split(':')[0]} className="w-16 h-16" style={{ backgroundColor: 'transparent' }} />
-                            <Typography color="gray" className="font-normal flex-1">
-                                {kudosDescriptions[index]}
+                        <div key={index} className="flex flex-col items-center gap-2 p-2">
+                            <img src={icon} alt={kudosDescriptions[index].split(':')[0]} className="w-24 h-24" />
+                            <Typography color="gray" className="font-normal text-center">
+                                <strong>{kudosDescriptions[index].split(':')[0]}</strong>: {kudosDescriptions[index].split(':')[1]}
                             </Typography>
                         </div>
                     ))}
