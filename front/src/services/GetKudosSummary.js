5ba842651b0a61bc9956311190de41d0e2807264
JorgeHadad <jorgemiguel.hadadmenegaz@externos-cl.cencosud.com>
2025-04-06 16:59:02 -0300

    create kudos services

---
+++ front/src/services/GetKudosSummary.js
@@ -0,0 +1,25 @@
+// services/GetKudosSummary.js
+export async function GetKudosSummary(token, idModule, idRegularUser = "") {
+  const endpoint = `http://localhost:8080/api/v1/summary/kudos?idModule=${idModule}` + 
+                   (idRegularUser ? `&idRegularUser=${idRegularUser}` : "");
+  try {
+    const response = await fetch(endpoint, {
+      method: "GET",
+      headers: {
+        "Authorization": `Bearer ${token}`,
+        "Accept": "application/json"
+      }
+    });
+    if (response.ok) {
+      const data = await response.json();
+      return data; // Array de { badgeName, numberOfBadges }
+    } else {
+      const errorData = await response.json();
+      console.error("Error en GetKudosSummary:", errorData);
+      return [];
+    }
+  } catch (error) {
+    console.error("Error en la solicitud GetKudosSummary:", error);
+    return [];
+  }
+}
