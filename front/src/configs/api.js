0eaee570694354d8fa0b06505ebf4fc38e6901c5
Matias Snaider <mati.iva.41@gmail.com>
2025-06-02 16:26:52 -0300

    host nube

--- front/src/configs/api.js
+++ front/src/configs/api.js
@@ -1,2 +1,3 @@
-export const host = "http://localhost:8080"
+// export const host = "http://localhost:8080"
+export const host = "https://feelflow-api.azurewebsites.net"
 export const frontHost = "http://localhost:5173"
