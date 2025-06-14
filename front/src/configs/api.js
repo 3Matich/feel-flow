b19f52dae70da12534c15592e9612ed4c6339c83
Matias Snaider <mati.iva.41@gmail.com>
2025-06-13 21:08:24 -0300

    url front cloud

--- front/src/configs/api.js
+++ front/src/configs/api.js
@@ -1,3 +1,4 @@
 // export const host = "http://localhost:8080"
 export const host = "https://feelflow-api.azurewebsites.net"
-export const frontHost = "http://localhost:5173"
+// export const frontHost = "http://localhost:5173"
+export const frontHost = "https://feel-flow-five.vercel.app"
