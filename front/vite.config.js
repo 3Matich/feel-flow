aa9fb6c1feddf9daeecc2c1e1a0bc9f07947e763
Matias Snaider <mati.iva.41@gmail.com>
2025-06-17 17:38:58 -0300

    fix f5 v4

--- front/vite.config.js
+++ front/vite.config.js
@@ -1,16 +1,20 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  define: {
-    global: 'window',
+  base: '/', // 👈 importante para rutas absolutas en producción
+  build: {
+    outDir: 'dist' // 👈 debe coincidir con el "distDir" del vercel.json
   },
   resolve: {
     alias: [
-      { find: "@", replacement: "/src" },
-      { find: "./window-config.mjs", replacement: path.resolve(__dirname, "src/dummy-config.js") },
+      { find: '@', replacement: '/src' },
+      { find: './window-config.mjs', replacement: path.resolve(__dirname, 'src/dummy-config.js') },
     ],
+  },
+  define: {
+    global: 'window',
   },
 });
