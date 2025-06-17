8e32514d70fe98c6cf798c50b04c35ae278f5694
Matias Snaider <mati.iva.41@gmail.com>
2025-06-17 20:40:13 -0300

    fix f5 v11.3

--- front/vite.config.js
+++ front/vite.config.js
@@ -1,12 +1,12 @@
-import path from 'path';
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: [
       { find: '@', replacement: '/src' },
-      // 👇 Esto redirige el import roto
       { find: './window-config.mjs', replacement: path.resolve(__dirname, 'src/dummy-config.js') }
     ]
   },
